f725fd17999697bc8eb26209f5c08123864aa68c
Dante1724 <imanuel.david@ti.ukdw.ac.id>
2020-10-01 20:42:50 +0700

    -add simple change

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MyActivity", "Check");
         setContentView(R.layout.activity_main);
     }
 }
